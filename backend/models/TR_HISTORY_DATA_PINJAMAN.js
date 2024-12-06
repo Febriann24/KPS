e185a56d5cca5a66ca7ee124256f52f8b7415bf1
lietristan <115028927+lietristan@users.noreply.github.com>
2024-12-06 12:13:47 +0700

    [UPD] fix uncommitted changes

---
+++ backend/models/TR_HISTORY_DATA_PINJAMAN.js
@@ -0,0 +1,59 @@
+import { DataTypes } from "sequelize";
+import db from "../config/database.js";
+
+const TrHistoryDataPinjaman = db.define("TR_HISTORY_DATA_PINJAMAN", {
+    UUID_HIS_PINJAMAN: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    DTM_CRT: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: true
+    },
+    USR_CRT: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    DTM_UPD: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    USR_UPD: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    ANGSURAN_BERBUNGA: {
+        type: DataTypes.BIGINT,
+        allowNull: true
+    },
+    ANGSURAN_BERSIH: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    UUID_PENGAJUAN_PINJAMAN: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        references: {
+            model: "TR_PENGAJUAN_PINJAMAN",
+            key: "UUID_PENGAJUAN_PINJAMAN"
+        }
+    }
+}, {
+    freezeTableName: true
+});
+
+(async () => {
+    const { default: TR_PENGAJUAN_PINJAMAN } = await import('./TR_PENGAJUAN_SIMPANAN.js');
+
+    TrHistoryDataPinjaman.belongsTo(TR_PENGAJUAN_PINJAMAN, {
+        foreignKey: 'UUID_PENGAJUAN_PINJAMAN',
+        as: 'TrPengajuanPinjaman'
+    });
+
+})();
+
+export default TrHistoryDataPinjaman;
+
+console.log("Creating TR_HISTORY_DATA_PINJAMAN");
